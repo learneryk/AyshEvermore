3da839bf9d2533542d35b2cdc869510965084968
learneryk <timtom2023@gmail.com>
2026-08-15 17:22:46 +0530

    Add Live Preview pane to Sanity CMS

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,7 +1,28 @@
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {structureTool, DefaultDocumentNodeResolver} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+
+import React from 'react'
+
+const PreviewIFrame = () => React.createElement('iframe', {
+  src: 'https://ayshevermore.pages.dev',
+  style: { width: '100%', height: '100%', border: 'none' }
+})
+
+const defaultDocumentNode: DefaultDocumentNodeResolver = (S, {schemaType}) => {
+  // Pages that correspond to visual pages on the website
+  const previewPages = ['homePage', 'aboutPage', 'servicesPage', 'galleryPage', 'contactPage']
+  
+  if (previewPages.includes(schemaType)) {
+    return S.document().views([
+      S.view.form(),
+      S.view.component(PreviewIFrame).title('Live Preview')
+    ])
+  }
+  
+  return S.document().views([S.view.form()])
+}
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +31,7 @@ export default defineConfig({
   projectId: 'ipnzqgln',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({ defaultDocumentNode }), visionTool()],
 
   schema: {
     types: schemaTypes,
